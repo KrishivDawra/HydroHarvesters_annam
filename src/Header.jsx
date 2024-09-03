6f4fac765606693fd87ecca6ba8de0441a2eddc0
KrishivDawra <krishivdawra@gmail.com>
2024-09-03 14:09:50 +0530

    little work

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='flex justify-between bg-green-400 align-baseline'>
       <img className='h-[90px] w-[110px] rounded-full size-48 ' src=".\src\assets\logo123-removebg-preview.png" alt="" />
-        <ul className='flex space-x-4 py-8 text-black '>
+        <ul className='lg:flex lg:space-x-4 lg:py-8 lg:text-black hidden'>
             <li className='font-serif font-semibold  hover:text-black'>Home</li>
             <li className='font-serif font-semibold  hover:text-black'>Farmer_Section</li>
             <li className='font-serif font-semibold  hover:text-black'>consultencies</li>
@@ -12,7 +12,7 @@ const Header = () => {
         </ul>
         <div className='py-5'>
             <a href="#" className='pr-4'>Login</a>
-            <button className='bg-green-400 px-4 py-2 rounded-xl hover:bg-green-500'>Join Us</button>
+            <button className='bg-gradient-to-r from-orange-400 to-red-500 px-4 py-2 rounded-xl '>Join Us</button>
         </div>
     </div>
   )
