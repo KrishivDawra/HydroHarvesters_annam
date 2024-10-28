32bf707ae08308e9229deb97fc3bb4374500301b
KrishivDawra <krishivdawra@gmail.com>
2024-10-29 03:14:34 +0530

    product page except calculator

--- src/products.jsx
+++ src/products.jsx
@@ -1,28 +1,11 @@
-import Header from "./Header";
-
-
 
 function farmer_section() {
   
 
     return (
       <>
-      <div >
-          <div className='bg-green-400'>
-              <Header/>
-          </div>
-        <div>
-          <h1>Future ready setups</h1>
-          <div>
-            <img src=".\src\assets\p-2.jpg" alt="" />
-            <p>Hydro Harvesters: Increase yields in small spaces with advanced soilless farming</p>
-          </div>
+      <div>
 
-          <div>
-            <img src=".\src\assets\p-1.jpg" alt="" />
-            <p>Maximize profits with Hydro Harvesters' advanced farming techniques</p>
-          </div>
-        </div>
       </div>
         
       </>
---
+++ src/scriptstore.js
@@ -0,0 +1,28 @@
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const area = document.getElementById("area").value;
+        const areaValue = document.getElementById("areaValue").value;
+        const type = document.getElementById("type").value;
+        const result = document.getElementById("result");
+
+        
+        if (!area || !areaValue || type === "Select") {
+            result.textContent = "Please select valid options and enter area.";
+            return;
+        }
+
+    
+        let profit = 0;
+        if (type === "Vertical") {
+            profit = areaValue * 10; 
+        } else if (type === "Horizontal") {
+            profit = areaValue * 7;
+        } else if (type === "A-Shape") {
+            profit = areaValue * 5;
+        } else if (type === "Hydro Harvesters Special") {
+            profit = areaValue * 15;
+        }
+        // Display result
+        result.textContent = `Estimated Profit: ₹${profit}`;
+    }
